eb3a4940547babf20ffa8685c8f395a555195e85
SimonAMcKenzie <simon-mckenzie@hotmail.co.uk>
2023-12-17 23:58:22 +0000

    created logic for each section of README to input response data

--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -1,7 +1,36 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(response) {
+  return `# ${response.title}
+  # ${reponse.title}
 
+  ## Description
+  ${reponse.description}
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+  ${response.installation}
+
+  ## Usage
+  ${response.usage}
+
+  ## License
+  This project is licensed under the ${response.license} license.
+
+  ## Contributing 
+  ${response.contributing}
+
+  ## Tests
+  ${response.tests}
+
+  ## Questions
+  For any questions, please get in touch [${response.username}](https://hithub.com/${responses.username}) or email at ${responses.email}.
 `;
 }
 
